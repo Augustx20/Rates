78719b66336ebb7b8840a8f4429cc6400bfa6e27
Machado <augusto.machado@kuehne-nagel.com>
2023-04-18 08:03:58 -0300

    Variable de tiempo

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const oanda = require('./Components/Oanda/Oanda')
 read.read()
 oanda.Oanda()
 
-setTimeout(() => {
-create.CreateExcel()
-}, 15000);
+function ejecutarProceso() {
+    const datos = read.data;
+    const cantidadDatos = datos.length;
+    let tiempoEspera = cantidadDatos * 2500; 
+  
+    tiempoEspera = Math.min(tiempoEspera, 100000);
+    setTimeout(() => {
+        create.CreateExcel()
+    }, tiempoEspera);
+    console.log(tiempoEspera)
+  }
+  
+  ejecutarProceso()
